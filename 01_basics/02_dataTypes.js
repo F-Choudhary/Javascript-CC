64e44b78b6efae7c8fa6fea95e7f4f719217cf3b
Fakhruddin Choudhary <72145757+F-Choudhary@users.noreply.github.com>
2023-11-21 16:04:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// Writing into an HTML element, 
+// using innerHTML.
+// Writing into the HTML output using 
+// document.write(3+3)
+// Writing into an alert box, using 
+// window.alert().
+// Writing into the browser console, using 
+// console.log().
+// console.log(3); console.log("fakhr"); // Not good practice
+console.log(3 
+    +
+     3) // Not good practice, code readability should be high
+
+console.log("Fakhruddin")
+
+let name = "Fakhrr"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// primitive datatypes
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
