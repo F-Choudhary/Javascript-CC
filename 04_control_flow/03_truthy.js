513f9df2aa9a45c9ff28f57a92cbeca95d4e1810
Fakhruddin Choudhary <72145757+F-Choudhary@users.noreply.github.com>
2023-12-05 16:40:32 +0000

    ternary operator

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -33,4 +33,10 @@ val1 = null ?? 10 ?? 20
 
 console.log(val1);
 
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
 
