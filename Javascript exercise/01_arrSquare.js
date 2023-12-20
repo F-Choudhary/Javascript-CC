373c811096e89b2ec08ece0c5c39da4cb301894b
Fakhruddin Choudhary <72145757+F-Choudhary@users.noreply.github.com>
2023-12-20 15:47:00 +0000

    map,forEach, math.pow()

--- Javascript exercise/01_arrSquare.js
+++ Javascript exercise/01_arrSquare.js
@@ -1,6 +1,7 @@
 // const input = [1, 2, 3, 4, 5];
 // console.log(input.forEach( (num) => Math.pow(num,2)))
 // console.log(input);
-const input = [1, -4, 12, 0, -3, 29, -150];
-const sum = input.filter( (num) => num > 0).reduce( (prev,curr) => prev+curr,0) 
-console.log(sum);
+
+const input = [1, 2, 3, 4, 5];
+let sqrt = input.map( (num) => Math.pow(num,2))
+console.log(sqrt);
